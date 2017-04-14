e5e02549d0e70a9de897940a5bc43a24c0a8477a
Sara <sklvgirl@gmail.com>
2017-04-14 14:44:09 -0700

    working model + stubs of other models

--- my_project/sara_eda_jokes.py
+++ my_project/sara_eda_jokes.py
@@ -3,14 +3,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import graphlab as gl
 
+#set up conda env
 # conda -V
 # conda create -n jokeenv python = 2.7 anaconda
 # source activate jokeenv
 # conda install -n jokeenv graphlab
 # source deactivate
 
-ratings = gl.SFrame(ratings_data_fname, format='tsv')
-sample_sub = pd.read_csv(sample_sub_fname)
-for_prediction = gl.SFrame(sample_sub)
+#import data
+jokes = gl.SFrame('data/jokes.dat', format='tsv', header=None)
+all_jokes = jokes.to_numpy().tolist()
+ratings = gl.SFrame('data/ratings.dat', format='tsv')
 
-jokes = gl.SFrame('data/jokes.dat', format='tsv')
+#clean joke code
+all_jokes = jokes.to_numpy().tolist()
+all_jokes = [item for sublist in all_jokes for item in sublist]
+clean_jokes = []
+marks = ['<p>', '/>','</p>']
+for joke in all_jokes:
+    if joke.split()[-1] in marks:
+        pass
+    else:
+        clean_jokes.append(joke)
